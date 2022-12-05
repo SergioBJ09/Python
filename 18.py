51869ebb1fdf2149fe492589dde6d00f391dfb66
SergioBJ09 <sbolivar19332@iesjoanramis.org>
2022-12-05 09:34:29 +0100

    Cambio Local

---
+++ 18.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+
+    first=[2,5,7,9]
+    second=[1,3,5,7]
+    common=set(first).intersection(second)
+    print("Tienen en comun" ,common)
---
+++ 19.py
@@ -0,0 +1,10 @@
+def crear_repetido(a):
+    sumatorio = 0
+    multiplicado=1
+    for a in lista:
+        multiplicado = b*c
+    return multiplicado
+
+b=print=input("Indique un numero: ")
+c=print=input("Indique un caracter: ")
+print()
