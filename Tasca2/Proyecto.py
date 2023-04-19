25f2f0922f4e73ac1feacf9de224011aedb623d0
SergioBJ09 <sbolivar19332@iesjoanramis.org>
2023-04-19 12:43:45 +0200

    Si

---
+++ Tasca2/Proyecto.py
@@ -0,0 +1,22 @@
+import random
+def menu_principal():
+    print("""
+    Menú:
+    1. 
+    2. 
+    3. 
+    0. Salir
+    """
+    )
+    opcio=input("Seleccione la opcion que quiera: ")
+    while(opcio!=0):
+        match opcio:
+            case "1":
+            case "2":
+            case "3":
+            case other:
+            
+            opcio=0
+
+#PP
+menu_principal()
---
+++ Tasca2/apuntes.py
@@ -0,0 +1,4 @@
+def pe(a,*args):
+def pi(b,**kwags):
+def suma(*lista):
+def contactes(**diccionario):
